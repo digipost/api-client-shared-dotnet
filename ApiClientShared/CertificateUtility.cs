a6ccd408ac6736f268986bd67048ed8ee8cdb69a
Kristian Enge <kristian.ienge@gmail.com>
2016-05-03 15:40:30 +0200

    Removes ByteOrderMark(BOM) if present when using certificate methods

--- ApiClientShared/CertificateUtility.cs
+++ ApiClientShared/CertificateUtility.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Management.Instrumentation;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using ApiClientShared.Enums;
 
 namespace ApiClientShared
@@ -9,69 +12,15 @@ namespace ApiClientShared
     {
         public static string EnglishErrorDescription = "Could not find certificate";
         public static string NorwegianErrorDescription = "Klarte ikke finne sertifikat";
-
-        private static string GetErrorMessage(string thumbprint, Language language)
-        {
-            switch (language)
-            {
-                case Language.English:
-                    return String.Format("Could not find certificate with thumbprint: {0}",thumbprint);
-                case Language.Norwegian:
-                    return String.Format("Klarte ikke finne sertifikat med thumbprint: {0}", thumbprint);
-
-                default:
-                    throw new ArgumentOutOfRangeException("language");
-            }
-        }
         
-        /// <summary>
-        /// Retrieves certificate from personal certificates (StoreName.My) from current user.
-        /// </summary>
-        /// <param name="thumbprint">The thumbprint of the certificate.</param>
-        /// <param name="errorMessageLanguage">Specifies the error message language if certificate is not found.</param>
-        /// <returns>The certifikcate</returns>
         public static X509Certificate2 SenderCertificate(string thumbprint, Language errorMessageLanguage)
         {
-            X509Store storeMy = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            X509Certificate2 tekniskAvsenderSertifikat;
-            try
-            {
-                storeMy.Open(OpenFlags.ReadOnly);
-                tekniskAvsenderSertifikat = storeMy.Certificates.Find(
-                    X509FindType.FindByThumbprint, thumbprint, true)[0];
-            }
-            catch (Exception e)
-            {
-                throw new InstanceNotFoundException(GetErrorMessage(thumbprint,errorMessageLanguage), e);
-            }
-            storeMy.Close();
-            return tekniskAvsenderSertifikat;
+            return new CertificateFactory().SenderCertificate(thumbprint, errorMessageLanguage);
         }
-
-        /// <summary>
-        /// Retrieves certificate from trusted People (StoreName.TrustedPeople) from current user.
-        /// </summary>
-        /// <param name="thumbprint">The thumbprint of the certificate.</param>
-        /// <param name="errorMessageLanguage">Specifies the error message language if certificate is not found.</param>
-        /// <returns>The certifikcate</returns>
+        
         public static X509Certificate2 ReceiverCertificate(string thumbprint, Language errorMessageLanguage)
         {
-            var storeTrusted = new X509Store(StoreName.TrustedPeople, StoreLocation.CurrentUser);
-            X509Certificate2 mottakerSertifikat;
-            try
-            {
-                storeTrusted.Open(OpenFlags.ReadOnly);
-                mottakerSertifikat =
-                    storeTrusted.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, true)[0];
-            }
-            catch (Exception e)
-            {
-                throw new InstanceNotFoundException(GetErrorMessage(thumbprint,errorMessageLanguage), e);
-            }
-            storeTrusted.Close();
-            return mottakerSertifikat;
+            return new CertificateFactory().ReceiverCertificate(thumbprint, errorMessageLanguage);
         }
-
-
     }
 }
