a9910c4b0212e15c0e09027abedb70835a410609
Kristian Enge <kristian.ienge@gmail.com>
2016-05-04 09:02:34 +0200

    Removal of factory, inline public static methods, cleanup tests

--- ApiClientShared/CertificateUtility.cs
+++ ApiClientShared/CertificateUtility.cs
@@ -1,10 +1,15 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Management.Instrumentation;
+using System.Security.Cryptography.X509Certificates;
 using ApiClientShared.Enums;
 
 namespace ApiClientShared
 {
     public class CertificateUtility
     {
+        internal virtual KeyStoreUtility KeyStoreUtil { get; set; } = new KeyStoreUtility();
+
+        internal virtual BomUtility BomUtil { get; set; } = new BomUtility();
 
         /// <summary>
         ///     Retrieves certificate from personal certificates (StoreName.My) from current user.
@@ -14,8 +19,28 @@ namespace ApiClientShared
         /// <returns>The certifikcate</returns>
         public static X509Certificate2 SenderCertificate(string thumbprint, Language errorMessageLanguage)
         {
-            return new CertificateFactory().SenderCertificate(thumbprint, errorMessageLanguage);
+            return new CertificateUtility().CreateSenderCertificate(thumbprint, errorMessageLanguage);
         }
+
+        internal X509Certificate2 CreateSenderCertificate(string thumbprint, Language errorMessageLanguage)
+        {
+            var storeMy = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            X509Certificate2 senderCertificate;
+
+            thumbprint = BomUtil.RemoveBom(thumbprint);
+
+            try
+            {
+                senderCertificate = KeyStoreUtil.FindCertificate(thumbprint, storeMy);
+            }
+            catch (Exception e)
+            {
+                throw new InstanceNotFoundException(GetErrorMessage(thumbprint, errorMessageLanguage), e);
+            }
+
+            return senderCertificate;
+        }
+
         /// <summary>
         ///     Retrieves certificate from trusted People (StoreName.TrustedPeople) from current user.
         /// </summary>
@@ -24,7 +49,38 @@ namespace ApiClientShared
         /// <returns>The certifikcate</returns>
         public static X509Certificate2 ReceiverCertificate(string thumbprint, Language errorMessageLanguage)
         {
-            return new CertificateFactory().ReceiverCertificate(thumbprint, errorMessageLanguage);
+            return new CertificateUtility().CreateReceiverCertificate(thumbprint, errorMessageLanguage);
+        }
+
+        internal X509Certificate2 CreateReceiverCertificate(string thumbprint, Language errorMessageLanguage)
+        {
+            var storeTrusted = new X509Store(StoreName.TrustedPeople, StoreLocation.CurrentUser);
+            X509Certificate2 receiverCertificate;
+
+            thumbprint = BomUtil.RemoveBom(thumbprint);
+            try
+            {
+                receiverCertificate = KeyStoreUtil.FindCertificate(thumbprint, storeTrusted);
+            }
+            catch (Exception e)
+            {
+                throw new InstanceNotFoundException(GetErrorMessage(thumbprint, errorMessageLanguage), e);
+            }
+            return receiverCertificate;
+        }
+
+        private string GetErrorMessage(string thumbprint, Language language)
+        {
+            switch (language)
+            {
+                case Language.English:
+                    return $"Could not find certificate with thumbprint: {thumbprint}";
+                case Language.Norwegian:
+                    return $"Klarte ikke finne sertifikat med thumbprint: {thumbprint}";
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(language));
+            }
         }
     }
 }
