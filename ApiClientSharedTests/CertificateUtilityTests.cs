52aa51ffbda7f73af5c27e51b1600a23b48f650a
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-10-12 14:37:53 +0200

    Legger til støtte for å laste sertifikater fra LocalMachine i tillegg til CurrentUser
    - Dette har vært forespurt flere ganger, sist i #67.
    - Gjør en liten opprydding her også for å øke lesbarhet og redusere duplisering i kode. For eksempel så er det litt tullete a hente alle sertifkater med en thumbprint og så ta den første ([0]). Dette førte tidligere til en nullpointer som ble fanget og kastet som en `InstanceNotFoundException`. Nå gjør vi det om til en liste og henter første eller null, og kaster exception til slutt om det ikke finnes i noen stores.
    
    - Adds cleanup style

--- ApiClientSharedTests/CertificateUtilityTests.cs
+++ ApiClientSharedTests/CertificateUtilityTests.cs
@@ -1,8 +1,9 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using ApiClientShared;
 using ApiClientShared.Enums;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using Xunit;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace ApiClientSharedTests
 {
@@ -11,30 +12,50 @@ namespace ApiClientSharedTests
         private const string StringWithBom = "‎30 5b 7d 02 e6 5e 65 5f de a8 20 65 9c 3a e0 f1 a8 4b 72 2c";
         private const string StringWithoutBom = "30 5b 7d 02 e6 5e 65 5f de a8 20 65 9c 3a e0 f1 a8 4b 72 2c";
 
+        private static Mock<BomUtility> BomUtilMock => new Mock<BomUtility>();
 
-        private Mock<BomUtility> BomUtilMock()
+        private static Mock<KeyStoreUtility> KeyStoreMock
         {
-            return new Mock<BomUtility>();
+            get
+            {
+                var keyStoreUtilityMock = new Mock<KeyStoreUtility>();
+                keyStoreUtilityMock
+                    .Setup(utility => utility.FindCertificate(It.IsAny<string>(), It.IsAny<X509Store>()))
+                    .Returns(new X509Certificate2());
+                return keyStoreUtilityMock;
+            }
         }
 
-        private Mock<KeyStoreUtility> KeyStoreMock()
+        private static X509Certificate2 GetCertificateFromMockedStore(StoreName storeName, StoreLocation storeLocation,
+            bool isSenderCertificate)
         {
-            var _keyStoreUtilityMock = new Mock<KeyStoreUtility>();
-            _keyStoreUtilityMock
-                .Setup(utility => utility.FindCertificate(It.IsAny<string>(), It.IsAny<X509Store>()))
+            const string thumbprint = "someUniqueThumbprint";
+
+            var keyStoreUtilityMock = new Mock<KeyStoreUtility>();
+            keyStoreUtilityMock
+                .Setup(utility => utility
+                        .FindCertificate(
+                            It.Is<string>(p => p == thumbprint),
+                            It.Is<X509Store>(p => (p.Location == storeLocation) && (p.Name == storeName.ToString())))
+                )
                 .Returns(new X509Certificate2());
-            return _keyStoreUtilityMock;
+
+            var certificateUtility = new CertificateUtility {KeyStoreUtility = keyStoreUtilityMock.Object};
+
+            return isSenderCertificate
+                ? certificateUtility.CreateSenderCertificate(thumbprint, Language.English)
+                : certificateUtility.CreateReceiverCertificate(thumbprint, Language.English);
         }
 
-        [TestClass]
+        
         public class SenderCertificateMethod : CertificateUtilityTests
         {
-            [TestMethod]
-            public void CallsRemoveBom()
+            [Fact]
+            public void Calls_remove_bom()
             {
                 //Arrange
-                var bomUtilityMock = BomUtilMock();
-                var keyStoreMock = KeyStoreMock();
+                var bomUtilityMock = BomUtilMock;
+                var keyStoreMock = KeyStoreMock;
 
                 var certificateUtility = new CertificateUtility
                 {
@@ -49,11 +70,11 @@ namespace ApiClientSharedTests
                 bomUtilityMock.Verify(utility => utility.RemoveBom(It.IsAny<string>()), Times.Once());
             }
 
-            [TestMethod]
-            public void ThumbprintWithoutBomShouldNotChange()
+            [Fact]
+            public void Thumbprint_without_bom_should_not_change()
             {
                 //Arrange
-                var keyStoreMock = KeyStoreMock();
+                var keyStoreMock = KeyStoreMock;
                 var certificateUtility = new CertificateUtility {KeyStoreUtility = keyStoreMock.Object};
 
                 //Act
@@ -63,17 +84,39 @@ namespace ApiClientSharedTests
                 Assert.IsNotNull(certificate);
                 keyStoreMock.Verify(utility => utility.FindCertificate(StringWithoutBom, It.IsAny<X509Store>()));
             }
+
+            [Fact]
+            public void Accesses_current_user_store()
+            {
+                const StoreName storeName = StoreName.My;
+                const StoreLocation storeLocation = StoreLocation.CurrentUser;
+
+                var certificate = GetCertificateFromMockedStore(storeName, storeLocation, true);
+
+                Assert.IsNotNull(certificate);
+            }
+
+            [Fact]
+            public void Accesses_local_machine_store()
+            {
+                const StoreName storeName = StoreName.My;
+                const StoreLocation storeLocation = StoreLocation.LocalMachine;
+
+                var certificate = GetCertificateFromMockedStore(storeName, storeLocation, true);
+
+                Assert.IsNotNull(certificate);
+            }
         }
 
-        [TestClass]
+        
         public class ReceiverCertificateMethod : CertificateUtilityTests
         {
-            [TestMethod]
-            public void CallsRemoveBom()
+            [Fact]
+            public void Calls_remove_bom()
             {
                 //Arrange
-                var bomUtilityMock = BomUtilMock();
-                var keyStoreMock = KeyStoreMock();
+                var bomUtilityMock = BomUtilMock;
+                var keyStoreMock = KeyStoreMock;
                 var certificateFactory = new CertificateUtility
                 {
                     BomUtility = bomUtilityMock.Object,
@@ -88,11 +131,11 @@ namespace ApiClientSharedTests
                 bomUtilityMock.Verify(utility => utility.RemoveBom(It.IsAny<string>()), Times.Once());
             }
 
-            [TestMethod]
-            public void ThumbprintWithoutBomShouldNotChange()
+            [Fact]
+            public void Thumbprint_without_bom_should_not_change()
             {
                 //Arrange
-                var keyStoreMock = KeyStoreMock();
+                var keyStoreMock = KeyStoreMock;
                 var certificateFactory = new CertificateUtility {KeyStoreUtility = keyStoreMock.Object};
 
                 //Act
@@ -101,6 +144,28 @@ namespace ApiClientSharedTests
                 //Assert
                 Assert.IsNotNull(certificate);
                 keyStoreMock.Verify(utility => utility.FindCertificate(StringWithoutBom, It.IsAny<X509Store>()));
+            }
+
+            [Fact]
+            public void Accesses_current_user_store()
+            {
+                const StoreName storeName = StoreName.TrustedPeople;
+                const StoreLocation storeLocation = StoreLocation.CurrentUser;
+
+                var certificate = GetCertificateFromMockedStore(storeName, storeLocation, false);
+
+                Assert.IsNotNull(certificate);
+            }
+
+            [Fact]
+            public void Accesses_local_machine_store()
+            {
+                const StoreName storeName = StoreName.TrustedPeople;
+                const StoreLocation storeLocation = StoreLocation.LocalMachine;
+
+                var certificate = GetCertificateFromMockedStore(storeName, storeLocation, false);
+
+                Assert.IsNotNull(certificate);
             }
         }
     }
