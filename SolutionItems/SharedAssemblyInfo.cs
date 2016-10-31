e60619e2d68563a6c132027270054648bba91c93
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-10-31 13:57:21 +0100

    Adds pointer to correct (and cleaned) SharedAssemblyinfo

--- SolutionItems/SharedAssemblyInfo.cs
+++ SolutionItems/SharedAssemblyInfo.cs
@@ -1,4 +1,11 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.*")]
+[assembly: AssemblyCompany("Digipost AS")]
+[assembly: AssemblyTrademark("Digipost AS")]
+[assembly: AssemblyProduct("Digipost API Client Shared")]
+[assembly: AssemblyDescription("Digipost Client library helper tools.")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("© 2015-2016 Digipost AS")]
+
