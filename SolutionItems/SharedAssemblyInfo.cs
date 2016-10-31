29991c5adc32267e325094df9a4c277ff5e3fa84
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-10-31 13:59:27 +0100

    Added AssemblyInformationalVersion as required

--- SolutionItems/SharedAssemblyInfo.cs
+++ SolutionItems/SharedAssemblyInfo.cs
@@ -6,6 +6,7 @@
 [assembly: AssemblyDescription("Digipost Client library helper tools.")]
 [assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyCopyright("© 2015-2016 Digipost AS")]
 
