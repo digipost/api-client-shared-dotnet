bab08aeaaba560c0a652c443a14df507f1a19b74
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-10-31 16:58:31 +0100

    Bumps version for new public key

--- SolutionItems/SharedAssemblyInfo.cs
+++ SolutionItems/SharedAssemblyInfo.cs
@@ -4,9 +4,9 @@
 [assembly: AssemblyTrademark("Digipost AS")]
 [assembly: AssemblyProduct("Digipost API Client Shared")]
 [assembly: AssemblyDescription("Digipost Client library helper tools.")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyCopyright("© 2015-2016 Digipost AS")]
 
