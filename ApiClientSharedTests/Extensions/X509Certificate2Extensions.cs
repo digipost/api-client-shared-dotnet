544ae3b6a3223a60c9621db55afa2dc4f18cb9a3
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2017-03-30 14:24:01 +0200

    Legger til X509Certificate2Extension fra difi-felles-utility-dotnet
    Av samme grunn som commit tidligere fra difi-felles-utility

---
+++ ApiClientSharedTests/Extensions/X509Certificate2Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using static Difi.Felles.Utility.Resources.Language.LanguageResource;
+using static Difi.Felles.Utility.Resources.Language.LanguageResourceKey;
+
+namespace Difi.Felles.Utility.Extensions
+{
+    public static class X509Certificate2Extensions
+    {
+        public static string ToShortString(this X509Certificate2 certificate, string extraInfo = "")
+        {
+            var shortStringWithPlaceholders = GetResource(CertificateShortDescription);
+            return string.Format(shortStringWithPlaceholders, certificate.Subject, certificate.Thumbprint, extraInfo);
+        }
+    }
+}
