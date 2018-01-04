cf07b9012bca039174fd1ae895596989d3e5d1c6
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-04 10:19:07 +0100

    Legg til appveyor build rått fra gammel branch

---
+++ appveyor-deploy/NugetRestore.ps1
@@ -0,0 +1,5 @@
+. $PSScriptRoot\Common.ps1
+
+Print-Step-Header "before_build: Restoring NuGet dependencies"
+
+nuget restore
---
+++ appveyor-deploy/TestPreamble.ps1
@@ -0,0 +1,3 @@
+. $PSScriptRoot\Common.ps1
+
+Print-Step-Header "test: Running tests in solution if any"
