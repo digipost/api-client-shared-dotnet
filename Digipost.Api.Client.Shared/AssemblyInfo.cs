15822eb77dd90c4b36f21aa6277243771b2b50a9
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-10 12:45:17 +0100

    Legg til manglende AssemblyInfo som refereres til 🤦‍♀️

---
+++ Digipost.Api.Client.Shared/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Posten Norge AS")]
+[assembly: AssemblyTrademark("Posten Norge AS")]
+[assembly: AssemblyProduct("Posten Signature API Client")]
+[assembly: AssemblyDescription("Posten Signature Client library for API integration.")]
+[assembly: AssemblyVersion("4.2.0.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
+[assembly: AssemblyInformationalVersion("4.2.0")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("© 2015-2017 Posten Norge AS")]
+
