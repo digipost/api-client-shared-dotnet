6e9f00d854b8b49ccf3e81faa1f0c6588b1d6f64
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-11 12:18:39 +0100

    Legger apt-get update i scriptfil og legger til kommentarer

--- deploy/apt-get-add-keys-and-update.sh
+++ deploy/apt-get-add-keys-and-update.sh
@@ -1,9 +1,6 @@
-#Add keys
-echo "apt-get-script"
+#apt-get: Install key and add mono source
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list
-echo "start cat"
-cat /etc/apt/sources.list.d/mono-official.list 
+echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.listecho "start cat"
 
-
-#Update
+#Update packages to get newest mono able to do nuget deploy
+ sudo apt-get update
