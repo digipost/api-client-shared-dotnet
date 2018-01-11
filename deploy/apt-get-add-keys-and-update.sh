3fa95b5d37e3ed9ae3ea9342842fe8c80e8fbfdd
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-11 10:01:56 +0100

    Sjekker om mono-official blir skrevet til

--- deploy/apt-get-add-keys-and-update.sh
+++ deploy/apt-get-add-keys-and-update.sh
@@ -1,5 +1,6 @@
 #Add keys
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+cat /etc/apt/sources.list.d/mono-official.list 
 
 #Update
