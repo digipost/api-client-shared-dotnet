197af8a8aa8dc253f5da719018f3af99261ba290
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-11 10:20:35 +0100

    Debug info for å finne ut hvorfor script ikke kjører

--- deploy/apt-get-add-keys-and-update.sh
+++ deploy/apt-get-add-keys-and-update.sh
@@ -1,4 +1,5 @@
 #Add keys
+echo "apt-get-script"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | tee /etc/apt/sources.list.d/mono-official.list
 cat /etc/apt/sources.list.d/mono-official.list 
