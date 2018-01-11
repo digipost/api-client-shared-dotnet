2adf49e2b27bb4572263abff7d545fa356899765
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-11 09:22:38 +0100

    Removes the - at start of lines in scripts

--- deploy/nuget-install.sh
+++ deploy/nuget-install.sh
@@ -1,2 +1,2 @@
-- wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-- mono nuget.exe install zero29 -version 1.0.0 -source https://nuget.org/api/v2/
+wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+mono nuget.exe install zero29 -version 1.0.0 -source https://nuget.org/api/v2/
--- deploy/nuspec-version-patcher.sh
+++ deploy/nuspec-version-patcher.sh
@@ -1,6 +1,9 @@
 #Find current assembly version
+# 1: Zero29 lists versions of all AssemblyInfo-files. There is two.
+# 2: Take the first one
+# 3: Grep with regex to retrieve the assembly version number (ex. 4.3.0.0) at end of line
 assemblyVersion=$(mono ./Zero29.1.0.0/tools/Zero29.exe -l | head -n 1 | egrep -o '(\d+.){3}\d+')
 
-#Patch assembly version in .nuspec
+#Patch assembly version number in .nuspec
 sed -i '' "s/VERSION_PLACEHOLDER/${assemblyVersion}/g" api-client-shared.nuspec
 
