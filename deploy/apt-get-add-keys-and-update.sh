07bd8b4ca131550679e1eb73c404763b41e3651f
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-11 10:34:44 +0100

    helgardering sudo

--- deploy/apt-get-add-keys-and-update.sh
+++ deploy/apt-get-add-keys-and-update.sh
@@ -1,7 +1,7 @@
 #Add keys
 echo "apt-get-script"
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | tee /etc/apt/sources.list.d/mono-official.list
+echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list
 echo "start cat"
 cat /etc/apt/sources.list.d/mono-official.list 
 echo "end cat"
