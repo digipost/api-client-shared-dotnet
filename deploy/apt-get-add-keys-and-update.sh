0c527df63abf1bdabaf7085860712f4330c89231
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-14 15:37:31 +0100

    2. Drar ut oppdatering av pakker til egen scriptfil

--- deploy/apt-get-add-keys-and-update.sh
+++ deploy/apt-get-add-keys-and-update.sh
@@ -3,4 +3,5 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list
 
 #Update packages to get newest mono able to do nuget deploy
+sudo apt-get clean
 sudo apt-get update
