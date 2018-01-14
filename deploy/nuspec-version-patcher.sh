0a17dae1e5224122ffedd8db7d520bf6a6b2631c
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-14 17:08:52 +0100

    Patch byggnummer med verdi fra Travis

--- deploy/nuspec-version-patcher.sh
+++ deploy/nuspec-version-patcher.sh
@@ -1,8 +1,12 @@
 #Find current assembly version
 # 1: Zero29 lists versions of all AssemblyInfo-files. There is two.
 # 2: Take the first one
-# 3: Grep with regex to retrieve the assembly version number (ex. 4.3.0.0) at end of line
-assemblyVersion=$(mono ./Zero29.1.0.0/tools/Zero29.exe -l | head -n 1 | egrep -o '(\d+.){3}\d+')
+# 3: Grep with regex to retrieve the assembly version number (ex. '4.3.0.0') at end of line and excluding build number,
+#    ending up with '4.3.0.'
+assemblyVersionWithoutBuildNumber=$(mono ./Zero29.1.0.0/tools/Zero29.exe -l | head -n 1 | egrep -o '(\d+.){3}')
+assemblyVersion="${assemblyVersionWithoutBuildNumber}${TRAVIS_BUILD_NUMBER}"
+
+echo "Patching version in .nuspec to ${assemblyVersion}!"
 
 #Patch assembly version number in .nuspec
 sed -i '' "s/VERSION_PLACEHOLDER/${assemblyVersion}/g" api-client-shared.nuspec
