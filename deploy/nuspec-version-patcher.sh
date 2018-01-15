aeb697a7bdb8fd2c5a24c65ec6827b31f59c720b
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-15 12:10:57 +0100

    ✅ 4. Patcher nuspec skikkelig

--- deploy/nuspec-version-patcher.sh
+++ deploy/nuspec-version-patcher.sh
@@ -3,16 +3,15 @@
 # 2: Take the first one
 # 3: Grep with regex to retrieve the assembly version number (ex. '4.3.0.0') at end of line and excluding build number,
 #    ending up with '4.3.0.'
+assemblyVersionWithoutBuildNumber=$(mono ./Zero29.1.0.0/tools/Zero29.exe -l | head -n 1 | egrep -o '([0-9].){3}')
 
 mono ./Zero29.1.0.0/tools/Zero29.exe -l
 
-assemblyVersionWithoutBuildNumber=$(mono ./Zero29.1.0.0/tools/Zero29.exe -l | head -n 1 | egrep -o '(\d+.){3}')
-
-echo "Assembly version found with version_patcher is ${assemblyVersionWithoutBuildNumber}"
+echo "Assembly version found with version_patcher is '${assemblyVersionWithoutBuildNumber}' (build number excluded)."
 
 assemblyVersion="${assemblyVersionWithoutBuildNumber}${TRAVIS_BUILD_NUMBER}"
 
-echo "Patching version in .nuspec to ${assemblyVersion}!"
+echo "Patching version in .nuspec to '${assemblyVersion}!'"
 
 #Patch assembly version number in .nuspec
 sed -i.originalfilebackup "s/VERSION_PLACEHOLDER/${assemblyVersion}/g" api-client-shared.nuspec
